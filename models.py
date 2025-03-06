d5186f9dbc75f04bd54b8139b404809d64235913
Ishaan Mewara <mewaraishaan1199@gmail.com>
2025-03-06 06:30:48 +0000

    new changes

--- models.py
+++ models.py
@@ -3,24 +3,29 @@ models.py
 
 This module defines the database models for the application.
 """
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    """Represents a user in the database."""
+    """
+    Represents a user in the database.
+
+    Attributes:
+        id (int): The primary key identifier for the user.
+        name (str): The name of the user.
+    """
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
     def to_dict(self):
-        """Returns the full name of the user."""
+        """
+        Converts the user object into a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the user.
+        """
         return {"id": self.id, "name": self.name}
-
-
-
-
-
-
-
 
