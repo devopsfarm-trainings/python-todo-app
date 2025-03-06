95caa19598940f33b8b91ce7c4ea08b9c5aa5d58
Ishaan Mewara <mewaraishaan1199@gmail.com>
2025-03-06 06:06:53 +0000

    fixing models.py code

--- models.py
+++ models.py
@@ -1,11 +1,19 @@
+"""
+models.py
+
+This module defines the database models for the application.
+"""
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    """Represents a user in the database."""
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
     def to_dict(self):
+        """Returns the full name of the user."""
         return {"id": self.id, "name": self.name}
 
