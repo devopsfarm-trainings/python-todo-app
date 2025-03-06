87a257832f010361fe36c227c73d8891617020a5
Harshit <harshit@gmail.com>
2025-03-06 07:32:39 +0000

    fixed the code in config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+"""
+Configuration file for database settings.
+"""
+
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
+DATABASE_URL = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
 
